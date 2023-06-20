1fea70e3c3b8c930d148bc71564f6e560d9ff226
Michał Goliński <3693729+michalg9@users.noreply.github.com>
2023-06-20 14:54:05 +0200

    Update s3.tf - acl fix

--- s3.tf
+++ s3.tf
@@ -1,7 +1,6 @@
 
 resource "aws_s3_bucket" "website_bucket" {
   bucket = "michal-test-${module.pet-module.pet_name}"
-  acl    = "public-read"
 
   force_destroy = true
 
@@ -9,6 +8,32 @@ resource "aws_s3_bucket" "website_bucket" {
     index_document = "index.html"
     error_document = "index.html"
   }
+}
+
+resource "aws_s3_bucket_ownership_controls" "example" {
+  bucket = aws_s3_bucket.website_bucket.id
+  rule {
+    object_ownership = "BucketOwnerPreferred"
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "example" {
+  bucket = aws_s3_bucket.website_bucket.id
+
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+}
+
+resource "aws_s3_bucket_acl" "example" {
+  depends_on = [
+    aws_s3_bucket_ownership_controls.example,
+    aws_s3_bucket_public_access_block.example,
+  ]
+
+  bucket = aws_s3_bucket.website_bucket.id
+  acl    = "public-read"
 }
 
 resource "aws_s3_bucket_policy" "website_bucket_policy" {
