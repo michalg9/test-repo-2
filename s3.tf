6a0f1edd732daafe6b8decc6510b306393202823
Michał Goliński <3693729+michalg9@users.noreply.github.com>
2023-06-20 15:06:18 +0200

    Update s3.tf - policy fix

--- s3.tf
+++ s3.tf
@@ -5,28 +5,6 @@ resource "aws_s3_bucket" "website_bucket" {
   force_destroy = true
 }
 
-resource "aws_s3_bucket_policy" "website_bucket_policy" {
-  bucket = aws_s3_bucket.website_bucket.id
-
-  policy = <<POLICY
-{
-  "Version": "2012-10-17",
-  "Id": "Public-Access",
-  "Statement": [
-    {
-      "Sid": "Allow-Public-Access-To-Bucket",
-      "Effect": "Allow",
-      "Principal": "*",
-      "Action": "s3:GetObject",
-      "Resource": [
-          "arn:aws:s3:::${aws_s3_bucket.website_bucket.bucket}/*"
-      ]
-    }
-  ]
-}
-POLICY
-}
-
 resource "aws_s3_bucket_object" "object" {
   bucket = aws_s3_bucket.website_bucket.bucket
   key    = "index.html"
